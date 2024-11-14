a66c0dfec9198150bcbadadd5d7dec2a559bcf43
Daniel Lee <leecheef2010@gmail.com>
2024-11-14 07:33:20 -0700

    Add comments to state file

--- state.js
+++ state.js
@@ -1,3 +1,8 @@
+// This serves as our in-memory data store.
+// In a real-world application that would need to persist data across multiple runs,
+// this could be replaced with a database.
+// But for our purposes, an ephemeral in-memory store is sufficient.
+
 let DIRECTORIES = {}
 
 export const readDirectories = () => {
